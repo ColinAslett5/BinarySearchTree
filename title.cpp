4c53670c255508feb11758a19f85eb78b7b5e5ef
robert aslett <robert aslett>
2017-04-09 20:33:14 -0700

    Final COmmit

--- title.cpp
+++ title.cpp
@@ -15,13 +15,43 @@ void seperate(char* input);
 Node* search(int key);
 Node* search(int key, Node* root);
 void print(Node* root, int dif);
+Node* delt(Node* root, int x);
 int main(){
   cout << "Welcome to Binary Search Tree, Enter a list of numbers" << endl;
   char input[200];
   cin.getline(input,200);
   seperate(input);
-  int dif = 0;
-  print(head,dif);
+  bool kg = true;
+  while(kg == true){
+    char i;
+    cout << "1 = print tree, 2 = add a number, 3 = delete a number, 4 = quit" << endl;
+    cin >> i;
+    //printing out the tree
+    if(i == '1'){
+      int dif = 0;
+      print(head,dif);
+    }
+    //adding a number
+    if(i == '2'){
+      int numb;
+      cout << "Enter a number: ";
+      cin >> numb;
+      cout << endl;
+      add(numb);
+    }
+    //deleting a part of the tree
+    if(i == '3'){
+      int numx;
+      cout << "Enter a number: ";
+      cin >> numx;
+      cout << endl;
+      delt(head,numx);
+    }//quitting
+    if(i == '4'){
+      kg = false;
+    }
+  }
+  return 0;
 }
 void seperate(char* input){
   int index = 0;
@@ -96,7 +126,7 @@ void print(Node* root, int dif){
   if(root == NULL){
     return;
   }
-  int i = 2;
+  int i = 3;
   dif += i;
   print(root->rchild,dif);
   for(int x = i;x < dif;x++){
@@ -104,4 +134,44 @@ void print(Node* root, int dif){
   }
   printf("%d\n",root->value);
   print(root->lchild,dif);
+}
+Node* delt(Node* root, int x){
+  if(root == NULL){
+    return root;
+  }
+  else if(x < root->value){
+    root->lchild = delt(root->lchild,x);
+  }
+  else if(x > root->value){
+    root->rchild = delt(root->rchild,x);
+  }
+  else{
+    //if the root value has no children
+    if(root->lchild == NULL && root->rchild == NULL){
+      delete root;
+      root = NULL;
+    }
+    //if the root value has no left child
+    else if(root->lchild == NULL){
+      Node* current = root;
+      root = root->rchild;
+      delete current;
+    }
+    //if the root value has no right child
+    else if(root->rchild == NULL){
+      Node* current = root;
+      root = root->lchild;
+      delete current;
+    }
+    //smallest node possible
+    else{
+      Node* current = root->rchild;
+      while(current->lchild != NULL){
+	current = current->lchild;
+      }
+      root->value = current->value;
+      root->rchild = delt(root->rchild,current->value);
+    }
+  }
+  return root;
 }
